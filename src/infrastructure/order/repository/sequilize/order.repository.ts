7e2c292025add3a01955b4cf1206aa9395d05873
Caio Fuccio <caio.fuccio@gmail.com>
2024-08-25 23:56:59 -0300

    feat: adds update order and tests

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,25 +1,48 @@
-import Order from "../../../../domain/checkout/entity/order";
-import OrderItemModel from "./order-item.model";
-import OrderModel from "./order.model";
+import Order from '../../../../domain/checkout/entity/order';
+import OrderItem from '../../../../domain/checkout/entity/order_item';
+import OrderItemModel from './order-item.model';
+import OrderModel from './order.model';
 
 export default class OrderRepository {
-  async create(entity: Order): Promise<void> {
-    await OrderModel.create(
-      {
-        id: entity.id,
-        customer_id: entity.customerId,
-        total: entity.total(),
-        items: entity.items.map((item) => ({
-          id: item.id,
-          name: item.name,
-          price: item.price,
-          product_id: item.productId,
-          quantity: item.quantity,
-        })),
-      },
-      {
-        include: [{ model: OrderItemModel }],
-      }
-    );
-  }
+    async create(entity: Order): Promise<void> {
+        await OrderModel.create(
+            {
+                id: entity.id,
+                customer_id: entity.customerId,
+                total: entity.total(),
+                items: entity.items.map((item) => ({
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    product_id: item.productId,
+                    quantity: item.quantity,
+                })),
+            },
+            {
+                include: [{ model: OrderItemModel }],
+            },
+        );
+    }
+
+    async update(entity: Order): Promise<void> {
+        entity.items.forEach(async (item) => {
+            await OrderItemModel.update(
+                {
+                    product_id: item.productId,
+                    order_id: entity.id,
+                    name: item.name,
+                    price: item.price,
+                    quantity: item.quantity,
+                },
+                { where: { id: item.id } },
+            );
+        });
+
+        await OrderModel.update(
+            {
+                total: entity.total(),
+            },
+            { where: { id: entity.id } },
+        );
+    }
 }
