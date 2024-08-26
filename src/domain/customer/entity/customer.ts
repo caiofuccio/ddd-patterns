b21c279a6b2374ae72a409aeb12ccfe5fcd28a91
Caio Fuccio <caio.fuccio@gmail.com>
2024-08-26 00:00:59 -0300

    feat: install eslint and prettier and prettify the whole project

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,72 +1,72 @@
-import Address from "../value-object/address";
+import Address from '../value-object/address';
 
 export default class Customer {
-  private _id: string;
-  private _name: string = "";
-  private _address!: Address;
-  private _active: boolean = false;
-  private _rewardPoints: number = 0;
+    private _id: string;
+    private _name: string = '';
+    private _address!: Address;
+    private _active: boolean = false;
+    private _rewardPoints: number = 0;
 
-  constructor(id: string, name: string) {
-    this._id = id;
-    this._name = name;
-    this.validate();
-  }
-
-  get id(): string {
-    return this._id;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  get rewardPoints(): number {
-    return this._rewardPoints;
-  }
-
-  validate() {
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
+    constructor(id: string, name: string) {
+        this._id = id;
+        this._name = name;
+        this.validate();
     }
-    if (this._name.length === 0) {
-      throw new Error("Name is required");
+
+    get id(): string {
+        return this._id;
     }
-  }
 
-  changeName(name: string) {
-    this._name = name;
-    this.validate();
-  }
-
-  get Address(): Address {
-    return this._address;
-  }
-  
-  changeAddress(address: Address) {
-    this._address = address;
-  }
-
-  isActive(): boolean {
-    return this._active;
-  }
-
-  activate() {
-    if (this._address === undefined) {
-      throw new Error("Address is mandatory to activate a customer");
+    get name(): string {
+        return this._name;
     }
-    this._active = true;
-  }
 
-  deactivate() {
-    this._active = false;
-  }
+    get rewardPoints(): number {
+        return this._rewardPoints;
+    }
 
-  addRewardPoints(points: number) {
-    this._rewardPoints += points;
-  }
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error('Id is required');
+        }
+        if (this._name.length === 0) {
+            throw new Error('Name is required');
+        }
+    }
 
-  set Address(address: Address) {
-    this._address = address;
-  }
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
+    }
+
+    get Address(): Address {
+        return this._address;
+    }
+
+    changeAddress(address: Address) {
+        this._address = address;
+    }
+
+    isActive(): boolean {
+        return this._active;
+    }
+
+    activate() {
+        if (this._address === undefined) {
+            throw new Error('Address is mandatory to activate a customer');
+        }
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+
+    addRewardPoints(points: number) {
+        this._rewardPoints += points;
+    }
+
+    set Address(address: Address) {
+        this._address = address;
+    }
 }
